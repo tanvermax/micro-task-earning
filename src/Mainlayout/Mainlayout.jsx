252bbe2d52a514637cad1be9fd796e48b5ca3312
tanver909 <tanveermahidi@gmial.com>
2025-01-17 15:47:33 +0600

    navber and fotter added

--- src/Mainlayout/Mainlayout.jsx
+++ src/Mainlayout/Mainlayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navber from '../Home/Navber';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Home/Footer';
 
 const Mainlayout = () => {
     return (
-        <div>
+        <div className=''>
             <Navber></Navber>
+            <div className='min-h-screen'>
             <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
